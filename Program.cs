57098d97fd5bb93c45c77264d19cdd239b34d348
ryuselmi <yuselmirodriguez24@gmail.com>
2024-07-23 09:06:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSC_205_EX7_10
+{
+    public class AbecedarianChecker
+    {
+        public static bool IsAbecedarian(string word)
+        {
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (word[i] > word[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            // Test cases
+            Console.WriteLine(IsAbecedarian("abdest")); // True
+            Console.WriteLine(IsAbecedarian("agnosy")); // True
+            Console.WriteLine(IsAbecedarian("biopsy")); // True
+            Console.WriteLine(IsAbecedarian("deux"));   // True
+            Console.WriteLine(IsAbecedarian("dimpsy")); // True
+            Console.WriteLine(IsAbecedarian("hello"));  // False
+
+            Console.ReadKey();
+        }
+    }
+}
